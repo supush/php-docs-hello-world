4257602d210765f05157f3eb69cc35fd08b58c37
supush <139359392+supush@users.noreply.github.com>
2023-07-12 09:45:01 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 
-echo "Hello World!";<br>
-echo "A M S Prabhashwara Atapattu";<br>
-echo "500218461";<br>
+echo "Hello World!";
+<br>
+echo "A M S Prabhashwara Atapattu";
+<br>
+echo "500218461";
+<br>
 
 
