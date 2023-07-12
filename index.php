fbbfb5de8b6009a8ef3d0b2da92b438dd13d3074
supush <139359392+supush@users.noreply.github.com>
2023-07-12 09:57:49 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-session_start();
-include_once 'header.php';
-?>
-</body>
-<h1>A M S Prabhashwara Atapattu</h1>
-<h2>500218461</h2>
-</html>
+
+echo "Hello World!";
+<br>
+  echo "A M S Prabhashwara Atapattu";
+<br>
+  echo "500218461";
